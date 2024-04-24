a74022861f467b11f58152ea28f7306a8439b06f
Feavy <quentin071100@live.fr>
2024-04-24 14:19:39 +0200

    Delete blog / update logo link.

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -40,13 +40,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -58,10 +52,12 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Chibi Engine',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Chibi Engine',
         src: 'img/logo.svg',
+        href: 'https://chibiengine.com',
+        target: '_self'
       },
       items: [
         {
@@ -70,9 +66,8 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/ChibiEngine/ChibiEngine',
           label: 'GitHub',
           position: 'right',
         },
@@ -111,12 +106,8 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/ChibiEngine/ChibiEngine',
             },
           ],
         },
